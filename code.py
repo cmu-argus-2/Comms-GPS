9dde2f126a5488a24600bec90c4220e80db6c6c6
Adrian_Walker <Adrian.Walker2 (at) gmail.com>
2024-10-28 23:35:37 -0400

    Current working code

--- code.py
+++ code.py
@@ -1,51 +1,54 @@
-
-# type: ignore
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple GPS module demonstration.
-# Will wait for a fix and print a message every second with the current location
-# and other details.
-# import time
+# GPS module demonstration.
+import time
 import board
 import busio
 
 import digitalio
 # import adafruit_gps
 import gps_driver
-import gps_parser
 
 # Create a serial connection for the GPS connection using default speed and
 # a slightly higher timeout (GPS modules typically update once a second).
-# These are the defaults you should use for the GPS FeatherWing.
-# For other boards set RX = GPS module TX, and TX = GPS module RX pins.
 uart = busio.UART(board.TX, board.RX, baudrate=115200, timeout=10)
 
+# Power the GPS module
 pin = digitalio.DigitalInOut(board.GPS_EN)
 pin.direction = digitalio.Direction.OUTPUT
 pin.value = True
 
 # Create a GPS module instance.
 # gps = adafruit_gps.GPS(uart, debug=True)  # Use UART/pyserial
-gps = gps_driver.GPS(uart, debug=True)  # Use UART/pyserial
+gps = gps_driver.GPS(uart, debug=False)  # Use UART/pyserial
+
+# Wait for 3 seconds to allow the GPS module to boot up
+time.sleep(3)
+gps.set_to_binary()
 
 # Main loop runs forever printing the location, etc. every second.
-# last_print = time.monotonic()
+last_print = time.monotonic()
 while True:
     # Make sure to call gps.update() every loop iteration and at least twice
     # as fast as data comes from the GPS unit (usually every second).
     # This returns a bool that's true if it parsed new data (you can ignore it
     # though if you don't care and instead look at the has_fix property).
-    gps.update()
+    updated = gps.update()
+
     # Every second print out current location details if there's a fix.
-    # current = time.monotonic()
-    # if current - last_print >= 1.0:
-    #     last_print = current
-    #     if not gps.data:
-    #         # Try again if we don't have a fix yet.
-    #         print("Waiting for data...")
-    #         continue
-    #     # We have a fix! (gps.has_fix is true)
-    #     # Print out details about the fix like location, date, etc.
-    #     print("=" * 40)  # Print a separator line.
-    #     print("GPS data: {0}".format(gps.data))
+    current = time.monotonic()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not updated:
+            print("No update")
+            continue
+        else:
+            print("Updated")
+            nav_data = gps.get_nav_data()
+            if nav_data["fix_mode"] < 2:
+                print("No fix")
+                continue
+            else:
+                print("Fix: {0}".format(nav_data["fix_mode"]))
+                print("Latitude: {0}".format(nav_data["latitude"]))
+                print("Longitude: {0}".format(nav_data["longitude"]))
+                print("Satellites: {0}".format(nav_data["number_of_sv"]))
+                print("=" * 40)  # Print a separator line.
--- lib/gps_driver.py
+++ lib/gps_driver.py
@@ -1,104 +1,100 @@
-"""
-GPS message receiver.
-
-This module will receive GPS messages from a serial UART and save their data
-to their respective message type fields.
-
-It may be necessary to add sending commands too.
-
-Author: Adrian Walker
-"""
-
 try:
-    from typing import Optional, Tuple, List
-    from typing_extensions import Literal
-    from busio import UART, I2C
+    from typing import Optional
+    from busio import UART
+    from circuitpython_typing import ReadableBuffer
 except ImportError:
     pass
 
 class GPS:
-    """GPS parsing module.  Can parse simple NMEA data sentences from serial
-    GPS modules to read latitude, longitude, and more.
-    """
-
-    # lint warning about too many statements disabled
-    # pylint: disable-msg=R0915
     def __init__(self, uart: UART, debug: bool = False) -> None:
         self._uart = uart
         self.debug = debug
-        self._GPGGA = None
-        self._GNGLL = None
-        self._GNGSA = None
-        self._GPGSV = None
-        self._GLGSV = None
-        self._GAGSV = None
-        self._GBGSV = None
-        self._GNRMC = None
-        self._GNVTG = None
-        self._GNZDA = None
-
+        self._msg = None
+        self._payload_len = 0
+        self._msg_id = 0
+        self._msg_cs = 0
+        self._payload = bytearray([0] * 58)
+        self._nav_data = {}
 
     def update(self) -> bool:
-        """Check for updated data from the GPS module and process it
-        accordingly.  Returns True if new data was processed, and False if
-        nothing new was received.
-        """
-
         try:
-            sentence = self._parse_sentence()
+            msg = self._parse_sentence()
         except UnicodeError:
-            return None
-        if sentence is None:
             return False
-        if self.debug:
-            print("Raw sentence:")
-            print(sentence)
+        if msg is None or len(msg) < 11:
+            return False
 
-        if sentence.startswith(b"$GPGGA"):
-            self._GPGGA = sentence
-        elif sentence.startswith(b"$GNGLL"):
-            self._GNGLL = sentence
-        elif sentence.startswith(b"$GNGSA"):
-            self._GNGSA = sentence
-        elif sentence.startswith(b"$GPGSV"):
-            self._GPGSV = sentence
-        elif sentence.startswith(b"$GLGSV"):
-            self._GLGSV = sentence
-        elif sentence.startswith(b"$GAGSV"):
-            self._GAGSV = sentence
-        elif sentence.startswith(b"$GBGSV"):
-            self._GBGSV = sentence
-        elif sentence.startswith(b"$GNRMC"):
-            self._GNRMC = sentence
-        elif sentence.startswith(b"$GNVTG"):
-            self._GNVTG = sentence
-        elif sentence.startswith(b"$GNZDA"):
-            self._GNZDA = sentence
-        else:
-            if self.debug:
-                print("Unknown sentence")
+        self._msg = [hex(i) for i in msg]
+        self._payload_len = ((msg[2] & 0xFF) << 8) | msg[3]
+        self._msg_id = msg[4]
+        self._msg_cs = msg[-3]
+        self._payload = bytearray(int(i, 16) for i in self._msg[4:-3])
+
+        if self._msg_id != 0xA8:
+            print("Invalid message ID, expected 0xA8, got: ", hex(self._msg_id))
+            return False
+
+        if self.debug:
+            print("Raw message: \n", self._msg)
+            print("Payload: \n", self._payload)
+
+        cs = 0
+        for i in self._payload:
+            cs ^= i
+        if cs != self._msg_cs:
+            print("Checksum failed!")
+            return False
+
+        # Populate _nav_data as a dictionary
+        self._nav_data = {
+            "message_id": self._payload[0],
+            "fix_mode": self._payload[1],
+            "number_of_sv": self._payload[2],
+            "gps_week": (self._payload[3] << 8) | self._payload[4],
+            "tow": (self._payload[5] << 24) | (self._payload[6] << 16) | (self._payload[7] << 8) | self._payload[8],
+            "latitude": (self._payload[9] << 24) | (self._payload[10] << 16) | (self._payload[11] << 8) | self._payload[12],
+            "longitude": (self._payload[13] << 24) | (self._payload[14] << 16) | (self._payload[15] << 8) | self._payload[16],
+            "ellipsoid_alt": (self._payload[17] << 24) | (self._payload[18] << 16) | (self._payload[19] << 8) | self._payload[20],
+            "mean_sea_lvl_alt": (self._payload[21] << 24) | (self._payload[22] << 16) | (self._payload[23] << 8) | self._payload[24],
+            "gdop": (self._payload[25] << 8) | self._payload[26],
+            "pdop": (self._payload[27] << 8) | self._payload[28],
+            "hdop": (self._payload[29] << 8) | self._payload[30],
+            "vdop": (self._payload[31] << 8) | self._payload[32],
+            "tdop": (self._payload[33] << 8) | self._payload[34],
+            "ecef_x": (self._payload[35] << 24) | (self._payload[36] << 16) | (self._payload[37] << 8) | self._payload[38],
+            "ecef_y": (self._payload[39] << 24) | (self._payload[40] << 16) | (self._payload[41] << 8) | self._payload[42],
+            "ecef_z": (self._payload[43] << 24) | (self._payload[44] << 16) | (self._payload[45] << 8) | self._payload[46],
+            "ecef_vx": (self._payload[47] << 24) | (self._payload[48] << 16) | (self._payload[49] << 8) | self._payload[50],
+            "ecef_vy": (self._payload[51] << 24) | (self._payload[52] << 16) | (self._payload[53] << 8) | self._payload[54],
+            "ecef_vz": (self._payload[55] << 24) | (self._payload[56] << 16) | (self._payload[57] << 8) | self._payload[58]
+        }
+
+        if self.debug:
+            print("Nav data: \n", self._nav_data)
 
         return True
 
+    def get_nav_data(self) -> dict:
+        """Returns the current navigation data as a dictionary."""
+        return self._nav_data
+
+    def write(self, bytestr: ReadableBuffer) -> Optional[int]:
+        return self._uart.write(bytestr)
+
+    def set_to_binary(self) -> None:
+        self.write(b"\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A")
+
     @property
     def in_waiting(self) -> int:
-        """Returns number of bytes available in UART read buffer"""
         return self._uart.in_waiting
 
     def readline(self) -> Optional[bytes]:
-        """Returns a newline terminated bytestring, must have timeout set for
-        the underlying UART or this will block forever!"""
         return self._uart.readline()
 
-    def _read_sentence(self) -> Optional[str]:
+    def _read_sentence(self) -> Optional[bytes]:
         if self.in_waiting < 11:
             return None
+        return self.readline()
 
-        sentence = self.readline()
-        return sentence
-
-    def _parse_sentence(self) -> Optional[str]:
-        sentence = self._read_sentence()
-
-        return sentence
-
+    def _parse_sentence(self) -> Optional[bytes]:
+        return self._read_sentence()
