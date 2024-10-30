c16a089b77e1daffce406a7290687c8c9e667f70
Adrian_Walker <Adrian.Walker2 (at) gmail.com>
2024-10-29 22:20:48 -0400

    Updated working version with better data parser

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 # GPS module demonstration.
+# type: ignore
 import time
 import board
 import busio
@@ -6,6 +7,8 @@ import busio
 import digitalio
 # import adafruit_gps
 import gps_driver
+
+DEBUG = False
 
 # Create a serial connection for the GPS connection using default speed and
 # a slightly higher timeout (GPS modules typically update once a second).
@@ -18,7 +21,7 @@ pin.value = True
 
 # Create a GPS module instance.
 # gps = adafruit_gps.GPS(uart, debug=True)  # Use UART/pyserial
-gps = gps_driver.GPS(uart, debug=False)  # Use UART/pyserial
+gps = gps_driver.GPS(uart, debug=DEBUG)  # Use UART/pyserial
 
 # Wait for 3 seconds to allow the GPS module to boot up
 time.sleep(3)
@@ -35,20 +38,13 @@ while True:
 
     # Every second print out current location details if there's a fix.
     current = time.monotonic()
-    if current - last_print >= 1.0:
+    if current - last_print >= 1:
         last_print = current
-        if not updated:
-            print("No update")
-            continue
+        if gps._nav_data["fix_mode"] == 0:
+                print("Waiting for fix...")
+                continue
         else:
             print("Updated")
             nav_data = gps.get_nav_data()
-            if nav_data["fix_mode"] < 2:
-                print("No fix")
-                continue
-            else:
-                print("Fix: {0}".format(nav_data["fix_mode"]))
-                print("Latitude: {0}".format(nav_data["latitude"]))
-                print("Longitude: {0}".format(nav_data["longitude"]))
-                print("Satellites: {0}".format(nav_data["number_of_sv"]))
-                print("=" * 40)  # Print a separator line.
+            parsed_data = gps.get_parsed_data()
+            gps.print_parsed_data()
